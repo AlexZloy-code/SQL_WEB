3443900c72e069666e92d01ce8a77607f2ff8107
AlexZloy-code <email@example.com>
2025-03-27 23:29:33 +0300

    Получение одной работы

--- data/jobs_api.py
+++ data/jobs_api.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, jsonify, request
+from werkzeug.exceptions import NotFound
 
 from . import db_session
 from .jobs import Jobs
+
 
 jobs_api = Blueprint("jobs_api",
                      __name__,
@@ -14,6 +16,17 @@ def get_jobs():
     jobs = db_sess.query(Jobs).all()
     return jsonify(
         {"jobs": [job.to_dict() for job in jobs]}
+    )
+
+
+@jobs_api.route("/api/jobs/<int:job_id>")
+def get_job(job_id):
+    db_sess = db_session.create_session()
+    job: Jobs = db_sess.get(Jobs, job_id)
+    if not job:
+        raise NotFound()
+    return jsonify(
+        {"jobs": [job.to_dict()]}
     )
 
 
